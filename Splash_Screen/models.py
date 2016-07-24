3b65cda9860596fecc7ff1fae7ac636cf1be3a98
aditya <aditya999123@gmail.com>
2016-07-24 14:59:52 +0530

    mysql linked

--- Splash_Screen/models.py
+++ Splash_Screen/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+#from django_mysql import models
 class splash_screen_model(models.Model):
 	app_version=models.PositiveSmallIntegerField()
 	compulsary_update=models.PositiveSmallIntegerField()
--- ecellapp/settings.py
+++ ecellapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+#import mysql
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Splash_Screen',
+#    'django_mysql',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -75,13 +76,22 @@ WSGI_APPLICATION = 'ecellapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
